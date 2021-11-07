48099e5a7d8fcde44c8c3728664a271b1ffe2c03
Aidar Valshin <Aidar_V2000@mail.ru>
2021-11-07 12:28:20 +0300

    First commit. Create project

---
+++ src/main/java/RU/MEPHI/ICIS/C17501/messenger/MessengerApplication.java
@@ -0,0 +1,13 @@
+package RU.MEPHI.ICIS.C17501.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RU/MEPHI/ICIS/C17501/messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package RU.MEPHI.ICIS.C17501.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
